d6e82e3dcc4f157e401d2e6af07d57d4ddd1fb26
amandashack <ajshack4@gmail.com>
2024-10-20 14:52:53 -0700

    aborting xarray plotting for now

--- photon_energy_scan.py
+++ photon_energy_scan.py
@@ -5,40 +5,34 @@ import arpys
 from load_xtc import *
 from pyimagetool import imagetool
 
-def plot_hv_scan(ds, tofs_list):
-    for i, (arg, ax) in enumerate(zip(args, axes.flatten())):
-        dims = list(arg.dims)
-        alignment = arg.attrs.get('alignment', 'N/A')
-        polarization = arg.attrs.get('polarization', 'N/A')
-        sample_name = arg.attrs.get('material_value', 'N/A')
-        photon_energy = arg.attrs.get('photon_energy', 'N/A')
 
-        # Downsample if needed
-        if "downsample" in kwargs.keys():
-            arg_ds = arg.arpes.downsample({dims[2]: kwargs["downsample"]})
-            arg_cut = arg_ds.sel({dims[2]: energy}, method='nearest')
-        else:
-            arg_cut = arg.sel({dims[2]: energy}, method='nearest')
+def plot_hv_scan(hv_scan_xar):
+    ports = hv_scan_xar.coords['ports'].values
+    num_ports = len(ports)
 
-        # Plot with or without colorbar
-        if "add_colorbar" in kwargs.keys():
-            arg_cut.plot(x=dims[0], y=dims[1], ax=ax, cmap=color, add_colorbar=kwargs['add_colorbar'])
-        else:
-            arg_cut.plot(x=dims[0], y=dims[1], ax=ax, cmap=color, add_colorbar=False)
+    # Determine the number of columns and rows for subplots
+    num_cols = 4  # Adjust as needed
+    num_rows = (num_ports + num_cols - 1) // num_cols
 
-        # Add the information text
-        info_text = (f"Sample: {sample_name}\nhv: {photon_energy} eV\nAlignment: {alignment}\n"
-                      f"Polarization: {polarization}\nBinding Energy: {energy}")
-        ax.text(0.95, 0.05, info_text, transform=ax.transAxes, fontsize=10, verticalalignment='bottom',
-                horizontalalignment='right')
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(4 * num_cols, 3 * num_rows))
+    axes = axes.flatten()
 
-        # Turn off the title for each axis
-        ax.set_title('')
-        ni += 1
-    for j in range(ni, num_rows * 2):
-        fig.delaxes(axes.flatten()[j])
-    # Adjust layout to prevent overlap
-    plt.tight_layout(rect=[0, 0, 1, 0.98])
+    for i, port in enumerate(ports):
+        data = hv_scan_xar.sel(ports=port)
+        ax = axes[i]
+
+        # Transpose data to match the axes
+        pcm = ax.pcolormesh(data.coords['scan_var'], data.coords['tof'], data.T, shading='auto')
+        ax.set_title(f'Port {port}')
+        ax.set_xlabel('Scan Variable')
+        ax.set_ylabel('Time of Flight')
+        fig.colorbar(pcm, ax=ax)
+
+    # Remove any unused subplots
+    for j in range(i + 1, len(axes)):
+        fig.delaxes(axes[j])
+
+    plt.tight_layout()
     plt.show()
 
 if __name__ == "__main__":
@@ -58,4 +52,4 @@ if __name__ == "__main__":
     scan_axis = run6.scan_var
     hv_scan_xar = xr.DataArray(wf_ports, coords={'tof': tof_axis, 'scan_var': scan_axis, 'ports': ports},
                                dims=['tof', 'scan_var', 'ports'])
-    imagetool(hv_scan_xar)
+    plot_hv_scan(hv_scan_xar)
